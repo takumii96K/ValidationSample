08918a4827813a1ccdf22411e04797b8dbafbf81
takumi <154777096+takumii96K@users.noreply.github.com>
2024-01-22 13:09:50 +0900

    fix

--- ValidationSample/src/main/java/com/example/demo/controller/ValidationController.java
+++ ValidationSample/src/main/java/com/example/demo/controller/ValidationController.java
@@ -1,14 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.form.CalcForm;
+import com.example.demo.validator.CalcValidator;
 
 @Controller
 public class ValidationController {
@@ -24,7 +28,6 @@ public class ValidationController {
 		return "entry";
 	}
 
-
 	@PostMapping("calc")
 	public String confirmView(@Validated CalcForm form, BindingResult bindingResult, Model model) {
 		//入力された場合
@@ -37,5 +40,16 @@ public class ValidationController {
 		//Modelに格納する
 		model.addAttribute("result", result);
 		return "confirm";
+	}
+	/**インジェクション*/
+	@Autowired
+	CalcValidator calcValidator;
+	
+	/**相関チェック登録*/
+	@InitBinder("calcForm") //Modelに格納されるローワーのチェックしたいクラス名（識別名）を指定する。
+	//※指定しない場合全てのオブジェクトが対象になり、対象外の場合例外になる。
+	public void initBinder(WebDataBinder webDataBinder) {
+		webDataBinder.addValidators(calcValidator);
+		//webDataBinderインターフェースのaddメソッドでバリテーション機能を相関チェックとして登録する
 	}
 }
